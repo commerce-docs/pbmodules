45e291cc69e3b06db0d236c683ee43e2ab13d21d
Bruce Denham <bdenham@adobe.com>
2020-05-11 08:38:14 -0500

    initial commit
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    initial commit
    
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    initial commit
    
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    deleting master in prep for review
    
    Initial review
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Adding Apache license in compliance with legal policy
    
    PB-484: Page Builder Scaffolding CLI
    
    Updates from review
    
    initial commit
    
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    initial commit
    
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    deleting master in prep for review
    
    Initial review
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Adding Apache license in compliance with legal policy
    
    PB-484: Page Builder Scaffolding CLI
    
    Updates from review
    
    deleting master in prep for review
    
    initial commit
    
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    initial commit
    
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    deleting master in prep for review
    
    Initial review
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Adding Apache license in compliance with legal policy
    
    PB-484: Page Builder Scaffolding CLI
    
    Updates from review
    
    deleting master in prep for review
    
    Initial review
    
    parent 80458cbd9ac4628ba805b24b3af37eaa4a61625d
    author Bruce Denham <bdenham@adobe.com> 1589896548 -0500
    committer Bruce Denham <bdenham@adobe.com> 1589896548 -0500
    
    parent abc10152724cf6a82cfae5f256034d3ee386b608
    author Bruce Denham <bdenham@adobe.com> 1589896432 -0500
    committer Bruce Denham <bdenham@adobe.com> 1589896432 -0500
    
    initial commit
    
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    initial commit
    
    Added README draft
    
    Added gitignore file.
    
    Removed node_modules from repo
    
    Modified Readme.
    
    Added all templates and less files to create an individual template for each content type
    
    deleting master in prep for review
    
    Initial review
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Adding Apache license in compliance with legal policy
    
    PB-484: Page Builder Scaffolding CLI
    
    Updates from review
    
    deleting master in prep for review
    
    Initial review
    
    PB-484: Page Builder Scaffolding CLI
    
    Fixing issues with package and gitignore
    
    PB-484: Page Builder Scaffolding CLI
    
    Updates from review
    
    Cleanup for custom content type
    
    Cleanup for custom

---
+++ README.md
@@ -0,0 +1,12 @@
+# Page Builder modules CLI
+
+This is a CLI for scaffolding both custom and extension modules for Page Builder content types.
+
+## Usage
+1. Clone or download the repo.
+1. CD into the pbmodules directory and run `npm install` to download package dependencies.
+1. Run `npm install -g pbmodules` to install the scaffolding CLI globally.
+1. Navigate to where you want to create your Page Builder module.
+1. Enter `pbmodules` at the command prompt and follow the instructions.
+
+Details for about this tool will be added soon.
---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const clear = require('clear');
+const chalk = require('chalk');
+const {textSync} = require('figlet');
+const questions = require('./lib/questions');
+const files = require('./lib/files');
+const placeholders = require('./lib/placeholders');
+
+// Clear terminal and display ASCII art
+clear();
+console.log(
+    chalk.greenBright(textSync("PB Modules", {horizontalLayout: "fitted", font: "Standard"}))
+);
+
+const start = async () => {
+    try {
+        const moduleAnswers = await questions.askModuleQuestions();
+        const {vendor, templateType, contentType} = moduleAnswers;
+
+        placeholders.setupPlaceholders(moduleAnswers, vendor, contentType);
+
+        files.copyTemplate(vendor, templateType, contentType);
+        files.replaceFileNamePlaceholders(moduleAnswers);
+        files.replaceFileContentPlaceholders(moduleAnswers);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        console.log('✔ Page Builder module complete!')
+    }
+};
+
+start();
